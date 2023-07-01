08c88990d0b1c23ef49d3559ce6ffbea5dc4bd4e
unknown <kingwebbe@gmail.com>
2023-07-01 16:35:03 -0400

    added function to get author name on submit btn

--- Assets/script/script.js
+++ Assets/script/script.js
@@ -19,11 +19,26 @@ function getPoem(search, select) {
         .then(function (data) {
             console.log(data)
             console.log(data[0].lines)
-            poemParagraph.textContent = data[0].lines;
-            poemTitle.textContent = data[0].title;
+             poemTitle.textContent = data[0].title;
             poetName.textContent = data[0].author;
-})  
-}
+            poemParagraph.textContent = data[0].lines;
+            // save the data of the first poem from the api response
+            var poemData = data[0];
+            // check if the entered author matches any author from api
+           
+            if(select ==="author"){
+            removeTextFunction(data[0].author)
+                
+            }else{
+                // if the search type is not author, update the poem title and author name normally
+                poemParagraph.textContent = poemData.lines;
+                poetName.textContent = poemData.author;
+            }
+           });  
+        }
+            //store the api response data in local storage
+          
+
 
 
 //Add event listener to button, function passes form info to API call function
@@ -31,11 +46,18 @@ document.getElementById("searchForm").addEventListener('submit', function(e) {
     //CHANGE THIS TO REFLECT THE FORM
     console.log('hi')
     e.preventDefault();
-    var searchTerm = document.querySelector('input[type=text]').value;
-    var selectField = document.querySelector('input[type=radio]:checked').value;
+     // check if the selected search type is author 
+     // if it is , sets content of paragraph with id of "poemParagraph" to an empty string
+     var searchType = document.querySelector('input[name="searchType"]:checked').value;
+    
+        var searchTerm = document.querySelector('input[type=text]').value;
+        var selectField = document.querySelector('input[type=radio]:checked').value;
 
-    getPoem(searchTerm, selectField)
+        getPoem(searchTerm, selectField)
+     
+    
 })
+
 // getPoem()
 
 //FOR LATER
@@ -77,3 +99,18 @@ function randomP() {
 //getPoem()
 document.getElementById("randomButton").addEventListener("click", randomP);
 
+// clear content function
+            function removeTextFunction(authorName) {
+            poemParagraph.textContent = "";
+        // check if the entered input matches any author from the api response
+              poemTitle.textContent = authorName;
+              poetName.textContent = "";
+    
+    // check if the entered input matches any author from the api response
+         
+ }
+
+  function getList(){
+    
+  }
+ 
