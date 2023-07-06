54f5cada6e9aa17c765fad07112d9f44507be5c7
Ziad Alfadl <alfadlziad@gmail.com>
2023-07-06 19:13:09 -0400

    Update script.js

--- Assets/script/script.js
+++ Assets/script/script.js
@@ -18,7 +18,6 @@ let current_page = 0;
 let rows = 10;
 var linkList = [];
 var listData = [];
-// getLocalStorage()
 
 function getPoem(search, select) {
     //Add inputs to modify base URL
@@ -59,39 +58,7 @@ function displayPoem(object) {
     localStorage.setItem("Poem Title", pTitle);
     localStorage.setItem("Poet Name", poetn);
     poemParagraph.style.visibility = "visible";
-    // save the data of the first poem from the api response
-
-    // check if the entered author matches any author from api
-
-
 };
-
-//store the api response data in local storage
-
-function clearPage() {
-    poemTitle.textContent = '';
-    poetName.textContent = '';
-    poemParagraph.textContent = ''
-}
-
-
-//Add event listener to button, function passes form info to API call function
-document.getElementById("searchForm").addEventListener('submit', function (e) {
-    //CHANGE THIS TO REFLECT THE FORM
-    e.preventDefault();
-    // check if the selected search type is author 
-    // if it is , sets content of paragraph with id of "poemParagraph" to an empty string
-    var searchType = document.querySelector('input[name="searchType"]:checked').value;
-
-    var searchTerm = document.querySelector('input[type=text]').value;
-    var selectField = document.querySelector('input[type=radio]:checked').value;
-
-    getPoem(searchTerm, selectField)
-
-
-})
-
-
 
 // clear content function
 function removeTextFunction(authorName) {
@@ -110,12 +77,9 @@ function makelist(data) {
         var link = document.createElement("a")
 
         link.innerHTML = data[i].title
-        //add way to tie api call to each link with appropriate title
-        //make sure it looks nice
-        //add pages for entries over 10?
+       
         listContainer.appendChild(listItem)
         listItem.appendChild(link)
-        // link.addEventListener('click', displayPoem(data))
     }
 }
 
@@ -159,14 +123,11 @@ function randomP() {
     fetch(rpoemUrl)
         .then(function (response) {
             if (response.status === 200) {
-                // console.log(response);
                 return response.json()
 
                     .then(function (data) {
-                        // console.log(data)
-                        // console.log(data[0].lines)
                         infoTest = data
-displayPoem(data);
+                        displayPoem(data);
                         localStorage.setItem("Poem Title", data[0].title)
                         localStorage.setItem("Poet Name", data[0].author)
                     });
@@ -192,7 +153,6 @@ contentDisplay.addEventListener('dblclick', function (e) {
 document.addEventListener('click', clearCard)
 //Add event listener to take user input and make API call to retrieve poem(s)
 document.getElementById("searchForm").addEventListener('submit', function (e) {
-    //CHANGE THIS TO REFLECT THE FORM
     e.preventDefault();
     var searchTerm = document.querySelector('input[type=text]').value;
     var selectField = document.querySelector('input[type=radio]:checked').value;
@@ -204,32 +164,23 @@ document.getElementById("searchForm").addEventListener('submit', function (e) {
 
 document.getElementById("randomButton").addEventListener("click", randomP);
 
-// document.getElementById("wordSubmitBtn").addEventListener("click", submitWord);
-
-
-
-//listTitle = link.innerHTML ; link
-//titleNewPoem = listItem
-
 function getList(data) {
-    //var listContainer = document.createElement('ul')
-    //poemParagraph.appendChild(listContainer)
-    //console.log(poemParagraph) // contains the entire container- ul, li, href of title
+
     poemParagraph.style.visibility= "visible";
     for (var i = 0; i < data.length; i++) {
         // creating elements poem paragraph links to the poems 
         var link = document.createElement('a')
-        //console.log(link) // the actual link title names
+        
         // made listed items to make 10 titles pop up as bullet points
 
         var listItem = document.createElement('li')
         link.innerHTML = data[i].title
-        // listContainer.appendChild(listItem)
+   
         listItem.appendChild(link)
         // added this to make links clickable 
         link.href = "#";
         listData.push(listItem);
-        //console.log(listData);
+
         (function (index) {
 
             // add click event listeners to each anchor tage to handle the selection
@@ -260,104 +211,14 @@ function Pagination() {
         listContainer.appendChild(paginatedItems[i]);
 
     }
-    // generatePageButton();
-}
-/*
-var newPage = listData.length / 10;
-var roundnewpage = Math.floor(newPage);
-
-//console.log(newPage);
-//displayListPoem(listData, listItem, rows, current_page);
-//listItem.innerHTML = "";
-//page--;
-let start = rows * current_page;
-let end = start + rows;
-console.log(listData);
-let paginatedItems = listData.slice(start, end);
-console.log(paginatedItems);
-//URLappend ul to poemparagraph
-for (let i = 0; i < paginatedItems.length; i++) {
-  // linkList, a, update textcontent, update href
-   poemParagraph.appendChild(paginatedItems);
    
 }
-*/
-
-/*
-function generatePageButton () {
-    var newPage = listData.length / 10;
-var roundnewpage = Math.floor(newPage);
-var nextbtn = document.createElement('button')
-nextbtn.setAttribute("id", "nextButton");
-nextbtn.textContent = "Next";
-var prevbtn = document.createElement('button')
-prevbtn.setAttribute("id", "prevButton");
-prevbtn.textContent = "Previous";
-poemParagraph.appendChild(prevbtn);
-poemParagraph.appendChild(nextbtn);
-var thisPage = document.createElement('p')
-thisPage.setAttribute("id", "thisPageNumber");
-thisPage.textContent = "Page " + current_page + " of " + roundnewpage;
-poemParagraph.appendChild(thisPage);
-if (current_page == 0) {
-    prevbtn.style.display = 'none';
-};
-if (current_page == roundnewpage) {
-    nextbtn.style.display = 'none';
-}
-document.getElementById("nextButton").addEventListener("click", function () {
-    if (current_page < roundnewpage) {
-        current_page++;
-        getList(data);
-    }}
-);
-document.getElementById("prevButton").addEventListener("click", function () {
-    if (current_page > 0) {
-        current_page--;
-        getList(data);
-    }
-});
-};
-
-    
-
-
-//console.log(listItem); // list item that contains the href within it. 
-//console.log(listItem.textContent); // the text of href titles from within the list
-
-        (function (index) {
-
-            // add click event listeners to each anchor tage to handle the selection
-            link.addEventListener("click", function (e) {
-                // prevent default link behavior
-                e.preventDefault();
-                populatePoem(data[index]);
-                // pass the selected poem object tho the populated function 
-            });
-
-        })(i);
-    }
-};
-
-function displayListPoem(items, title, rows_per_page, page) {
-    title.innerHTML = "";
-   //page--;
-    let start = rows_per_page * page;
-    let end = start + rows_per_page;
-    let paginatedItems = items.slice(start, end);
-    console.log(paginatedItems);
-    for (let i = 0; i < paginatedItems.length; i++) {
-        poemParagraph.textContent = paginatedItems;
-        }
-    }
-*/
 
 //add the populatePoem function to set the selected poem lines in the poemParagraph
 function populatePoem(poemObject) {
     var linebreak = "<br>";
     poemTitle.textContent = poemObject.title;
     poetName.textContent = poemObject.author;
-    //poemParagraph.textContent = poemObject.lines;
     poemParagraph.innerHTML = poemObject.lines.join(linebreak);
     localStorage.setItem("Poem Title", poemObject.title)
     localStorage.setItem("Poet Name", poemObject.author)
@@ -396,13 +257,9 @@ function getLocalStorage() {
                     return response.json()
 
                         .then(function (data) {
-                            // if (data.status === 200) {
-                            //console.log(data[0].lines)
+                            
                             displayPoem(data)
-                            //} else {
-                            //   console.log("no previous stored poem for display")
-                            //   return;
-                            // }
+                            
                         });
                 }
             })
@@ -411,6 +268,6 @@ function getLocalStorage() {
     }
 };
 
-//poemParagraph.textContent = storage;
+
 
 getLocalStorage();
